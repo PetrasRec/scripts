08067c79b32d3a517cc7a47e5bfd0a92210cb939
Petras Viciunas <petrasviciunas@gmail.com>
2021-10-11 18:50:02 +0900

    Testing if workflow activates when there's a change in test folder

--- test/example/example.c
+++ test/example/example.c
@@ -779,6 +779,8 @@ void execute_one(void)
 }
 int main(void)
 {
+
+  
   syscall(__NR_mmap, 0x1ffff000ul, 0x1000ul, 0ul, 0x32ul, -1, 0ul);
   syscall(__NR_mmap, 0x20000000ul, 0x1000000ul, 7ul, 0x32ul, -1, 0ul);
   syscall(__NR_mmap, 0x21000000ul, 0x1000ul, 0ul, 0x32ul, -1, 0ul);
